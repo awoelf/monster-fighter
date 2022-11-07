d81759983c9a8d459773b494a892c69bf30659dd
awoelf <98348782+awoelf@users.noreply.github.com>
2022-11-07 14:12:50 -0600

    Finished card handlebar, menu, homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // Route for the homepage
 router.get('/', async (req, res) => {
-    res.render('homepage');
+    res.render('dashboard');
 })
 
 module.exports = router;
